b3f9ea285881aedd56905342e566ae21a24f7e18
Dinesh V <tgvdinesh@gmail.com>
2018-05-06 11:58:54 +0530

    Add Description & Home page URL Fixing disabled issue

--- static/content.js
+++ static/content.js
@@ -8,3 +8,8 @@ chrome.runtime.onMessage.addListener(function () {
     console.log(message);
     console.log(promise);
 });
+
+//TODO: Extension is disabled by default and not working for content_scripts/matches. Need a Fix!!
+chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    chrome.pageAction.show(tabs[0].id);
+});
